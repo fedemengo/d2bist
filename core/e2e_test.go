a3d1963b068ae30ec00c1d52e7084f7be8ab989a
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2022-12-29 18:23:05 +0100

    add tests

--- core/e2e_test.go
+++ core/e2e_test.go
@@ -31,6 +31,22 @@ func resultToBinStr(res *types.Result) (io.Reader, error) {
 	return bytes.NewReader([]byte(s)), nil
 }
 
+func compressData(data []byte, cType compression.CompressionType) []byte {
+	buf := new(bytes.Buffer)
+	cw, err := compression.NewCompressedWriter(context.Background(), buf, cType)
+	if err != nil {
+		return nil
+	}
+
+	cw.Write(data)
+
+	if err := cw.Close(); err != nil {
+		return nil
+	}
+
+	return buf.Bytes()
+}
+
 func traceLogger() zerolog.Logger {
 	level := zerolog.Disabled
 	logWriter := zerolog.ConsoleWriter{
@@ -51,15 +67,15 @@ func TestE2E(t *testing.T) {
 
 	testCases := []struct {
 		name         string
-		data         string
+		data         []byte
 		ops          []op
 		converters   []converter
 		opts         [][]Opt
-		expectedData string
+		expectedData []byte
 	}{
 		{
 			name: "decode/encode",
-			data: "dead beef",
+			data: []byte("dead beef"),
 			ops: []op{
 				Decode,
 				Encode,
@@ -72,10 +88,10 @@ func TestE2E(t *testing.T) {
 				{},
 				{},
 			},
-			expectedData: "dead beef",
+			expectedData: []byte("dead beef"),
 		}, {
-			name: "decode compress/encode decompress",
-			data: "dead beef",
+			name: "decode and compress/encode compressed",
+			data: []byte("dead beef"),
 			ops: []op{
 				Decode,
 				Encode,
@@ -88,10 +104,10 @@ func TestE2E(t *testing.T) {
 				{WithOutCompression(compression.Brotli)},
 				{WithInCompression(compression.Brotli)},
 			},
-			expectedData: "dead beef",
+			expectedData: []byte("dead beef"),
 		}, {
 			name: "decode and cap/encode",
-			data: "dead beef",
+			data: []byte("dead beef"),
 			ops: []op{
 				Decode,
 				Encode,
@@ -104,10 +120,10 @@ func TestE2E(t *testing.T) {
 				{WithOutBitsCap(32)},
 				{},
 			},
-			expectedData: "dead",
+			expectedData: []byte("dead"),
 		}, {
-			name: "decode compress/encode decompress and cap",
-			data: "dead beef",
+			name: "decode and compress/encode compressed and cap",
+			data: []byte("dead beef"),
 			ops: []op{
 				Decode,
 				Encode,
@@ -120,7 +136,86 @@ func TestE2E(t *testing.T) {
 				{WithOutCompression(compression.Brotli)},
 				{WithInCompression(compression.Brotli), WithOutBitsCap(16)},
 			},
-			expectedData: "de",
+			expectedData: []byte("de"),
+		}, {
+			name: "decode compressed/encode compressed and compress 2",
+			data: compressData([]byte("a longer text so that compression actually does something nice"), compression.Brotli),
+			ops: []op{
+				Decode,
+				Encode,
+			},
+			converters: []converter{
+				resultToBinStr,
+				basicConverter,
+			},
+			opts: [][]Opt{
+				{},
+				{WithInCompression(compression.Brotli), WithOutCompression(compression.Zstd), WithOutBitsCap(16 * 8)},
+			},
+			expectedData: []byte{0x28, 0xb5, 0x2f, 0xfd, 0x4, 0x0, 0x81, 0x0, 0x0, 0x61, 0x20, 0x6c, 0x6f, 0x6e, 0x67, 0x65},
+		}, {
+			name: "decode compressed/encode and compress",
+			data: compressData([]byte("a longer text so that compression actually does something nice"), compression.Brotli),
+			ops: []op{
+				Decode,
+				Encode,
+			},
+			converters: []converter{
+				resultToBinStr,
+				basicConverter,
+			},
+			opts: [][]Opt{
+				{},
+				{WithOutCompression(compression.Zstd), WithOutBitsCap(16 * 8)},
+			},
+			expectedData: []byte{0x28, 0xb5, 0x2f, 0xfd, 0x4, 0x0, 0x81, 0x0, 0x0, 0x1b, 0x3d, 0x0, 0xe8, 0xa5, 0xc3, 0xc8},
+		}, {
+			name: "decode (compressed 1 + 2) and decompress 1/encode (compressed 2) and decompress",
+			data: compressData(
+				compressData(
+					[]byte("a longer text so that compression actually does something nice"),
+					compression.S2,
+				),
+				compression.Brotli,
+			),
+			ops: []op{
+				Decode,
+				Encode,
+			},
+			converters: []converter{
+				resultToBinStr,
+				basicConverter,
+			},
+			opts: [][]Opt{
+				{WithInCompression(compression.Brotli)},
+				{WithInCompression(compression.S2)},
+			},
+			expectedData: []byte("a longer text so that compression actually does something nice"),
+		}, {
+			name: "decode (compressed 1 + 2 + 3) and decompress 1/encode (compressed 2) and decompress 2",
+			data: compressData(
+				compressData(
+					compressData(
+						[]byte("a longer text so that compression actually does something nice"),
+						compression.Gzip,
+					),
+					compression.Brotli,
+				),
+				compression.Zstd,
+			),
+			ops: []op{
+				Decode,
+				Encode,
+			},
+			converters: []converter{
+				resultToBinStr,
+				basicConverter,
+			},
+			opts: [][]Opt{
+				{WithInCompression(compression.Zstd)},
+				{WithInCompression(compression.Brotli)},
+			},
+			expectedData: compressData([]byte("a longer text so that compression actually does something nice"), compression.Gzip),
 		},
 	}
 
@@ -146,7 +241,7 @@ func TestE2E(t *testing.T) {
 			data, err := io.ReadAll(reader)
 			r.NoError(err)
 
-			a.Equal(tc.expectedData, string(data))
+			a.Equal(tc.expectedData, data)
 		})
 	}
 
